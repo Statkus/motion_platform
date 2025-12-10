b9a71ff1a8f698cd18d15856efcc0ede80b50f8e
Léo Statkus <leo@statkus.fr>
2025-12-10 15:48:09 +0100

    [DEV] Update timer used for fan and shaker

--- software/Src/main.c
+++ software/Src/main.c
@@ -309,10 +309,11 @@ int main(void)
 
   HAL_TIM_Base_Start(&htim3);
 
-  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
-  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_2);
 
-  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 500);
+  __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, 500);
+  __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_2, 0);
 
   /* USER CODE END 2 */
 
@@ -819,8 +820,8 @@ void Receive_Commands(void)
 
     m_target_speed = MAX(MIN((max_dist * 1000000) / MESSAGE_PERIOD_US, M_MAX_SPEED), -M_MAX_SPEED);
 
-    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 500 + (Get_Shaker_PWM() * 2));
-    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, Get_Fan_PWM() * 4);
+    __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_1, 500 + (Get_Shaker_PWM() * 2));
+    __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_2, Get_Fan_PWM() * 4);
   }
 }
 
